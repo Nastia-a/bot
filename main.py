9f1567495ac58e797f8553ca09ea19c73653254d
Nastia-a <anasdegtyareva@yandex.ru>
2026-03-25 13:39:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import logging
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 from config import Config
 from handlers.start import start_command, help_command
-from handlers.download import handle_message
+from handlers.download import handle_message, handle_callback
 
 # Configure logging
 logging.basicConfig(
@@ -28,9 +28,12 @@ def main():
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     
+    # ⭐ ВАЖНО: Добавляем обработчик нажатий на кнопки
+    application.add_handler(CallbackQueryHandler(handle_callback))
+    
     # Start bot
     logger.info("Bot started...")
-    application.run_polling(allowed_updates=["message"])
+    application.run_polling(allowed_updates=["message", "callback_query"])
 
 if __name__ == '__main__':
     main()
